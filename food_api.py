28d50bd4d458cebf851e5a4bb8b253ebb09eb5b3
Luke Braby <lbraby0314@gmail.com>
2024-04-13 18:00:32 -0400

    database and apis complete

---
+++ food_api.py
@@ -0,0 +1,155 @@
+import json
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/ingredients/search/<search>/<int:count>", methods=["GET"])
+def search_ingredients(search, count):
+    search = search.replace("_", " ") # for spaces use underscores
+
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    output = []
+
+    cur.execute("select * from ingredients where name like ? limit ?", ("%" + search + "%", count))
+    rows = cur.fetchall()
+    for row in rows:
+        output.append({"id": row[0], "name": row[1]})
+
+    cur.close()
+    conn.close()
+
+    return jsonify(output)
+
+@app.route("/dishes/search/<search>/<int:count>", methods=["GET"])
+def search_dishes(search, count):
+    search = search.replace("_", " ") # for spaces use underscores
+
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    output = []
+
+    cur.execute("select * from dishes where name like ? limit ?", ("%" + search + "%", count))
+    rows = cur.fetchall()
+    for row in rows:
+        output.append({"id": row[0], "name": row[1], "description": row[2], "uri": row[3], "image_url": row[4], "cuisine_type": row[5]})
+
+    cur.close()
+    conn.close()
+
+    return jsonify(output)
+
+@app.route("/dishes/cuisine_type/<cuisine_type>", methods=["GET"])
+def get_cuisine_dishes(cuisine_type):
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    output = []
+
+    cuisine_dishes = []
+    cur.execute("select * from dishes where cuisine_type = ?", (cuisine_type, ))
+    rows = cur.fetchall()
+    for row in rows:
+        cuisine_dishes.append(row)
+    
+    for dish in cuisine_dishes:
+        ingredients = []
+
+        cur.execute("select b.id, b.name from dish_ingredients a, ingredients b where a.ingredient_id = b.id and a.dish_id = ?", (dish[0], ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+            ingredients.append({"id": row[0], "ingredient": row[1]})
+
+        output.append({"id": dish[0], "name": dish[1], "description": dish[2], "ingredients": ingredients, "uri": dish[3], "image_url": dish[4], "cuisine_type": dish[5]})
+
+    cur.close()
+    conn.close()
+
+    return jsonify(output)
+
+@app.route("/dishes/your_menu", methods=["GET"])
+def get_menu():
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    output = []
+
+    your_menu = []
+    cur.execute("select * from dishes where name = ? or name = ? or name = ?", ("Sfiha", "Mulukhiyah", "Shawarma"))
+    rows = cur.fetchall()
+    for row in rows:
+        your_menu.append(row)
+    
+    for dish in your_menu:
+        ingredients = []
+
+        cur.execute("select b.id, b.name from dish_ingredients a, ingredients b where a.ingredient_id = b.id and a.dish_id = ?", (dish[0], ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+            ingredients.append({"id": row[0], "ingredient": row[1]})
+
+        output.append({"id": dish[0], "name": dish[1], "description": dish[2], "ingredients": ingredients, "uri": dish[3], "image_url": dish[4], "cuisine_type": "Lebanese"})
+
+    cur.close()
+    conn.close()
+
+    return jsonify(output)
+
+@app.route("/dishes/like/<username>/<dish_id>", methods=["POST"])
+def like_dish(username, dish_id):
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    liked_ingredients = []
+    cur.execute("select * from dish_ingredients where dish_id = ?", (dish_id, ))
+    rows = cur.fetchall()
+    for row in rows:
+        liked_ingredients.append(row[2])
+
+    for ingredient_id in liked_ingredients:
+        cur.execute("insert into liked_ingredients (username, ingredient_id) values (?, ?)", (username, ingredient_id))
+
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    return jsonify({"status": "success"})
+
+@app.route("/ingredients/like/<username>/<ingredient_id>", methods=["POST"])
+def like_ingredient(username, ingredient_id):
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    cur.execute("insert into liked_ingredients (username, ingredient_id) values (?, ?)", (username, ingredient_id))
+
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    return jsonify({"status": "success"})
+
+@app.route("/ingredients/liked/<username>", methods=["GET"])
+def get_liked_ingredients(username):
+    conn = sqlite3.connect('food_data.db')
+    cur = conn.cursor()
+
+    output = []
+
+    cur.execute("select b.id, b.name from liked_ingredients a, ingredients b where a.ingredient_id = b.id and a.username = ?", (username, ))
+    rows = cur.fetchall()
+    for row in rows:
+        output.append({"id": row[0], "name": row[1]})
+
+
+    cur.close()
+    conn.close()
+
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000) # host="0.0.0.0" makes Flask app accessible from external hosts
--- setup_database.py
+++ setup_database.py
@@ -14,7 +14,7 @@ cur.execute("create table ingredients (id INTEGER PRIMARY KEY, name TEXT)")
 with open("WorldFood/ingredients.json", "r", encoding="utf-8") as ingredients_file:
     ingredients = json.load(ingredients_file)
     for ingredient in ingredients:
-        cur.execute(f"insert into ingredients (name) values (?)", (ingredient,))
+        cur.execute("insert into ingredients (name) values (?)", (ingredient,))
 
 # create dishes table
 cur.execute("DROP TABLE IF EXISTS dishes")
@@ -25,7 +25,7 @@ with open("WorldFood/dishes.json", "r", encoding="utf-8") as dishes_file:
     dishes = json.load(dishes_file)
     for dish in dishes:
         dish_data = dishes[dish]
-        cur.execute(f"insert into dishes (name, description, uri, image_url, cuisine_type) values (?, ?, ?, ?, ?)", (dish, dish_data["description"], dish_data["uri"], dish_data["image_url"], dish_data["cuisine_type"]))
+        cur.execute("insert into dishes (name, description, uri, image_url, cuisine_type) values (?, ?, ?, ?, ?)", (dish, dish_data["description"], dish_data["uri"], dish_data["image_url"], dish_data["cuisine_type"]))
 
 # create dish_ingredients table
 cur.execute("DROP TABLE IF EXISTS dish_ingredients")
@@ -35,7 +35,7 @@ cur.execute("create table dish_ingredients (id INTEGER PRIMARY KEY, dish_id INTE
 with open("WorldFood/dishes.json", "r", encoding="utf-8") as dishes_file:
     dishes = json.load(dishes_file)
     for dish in dishes:
-        cur.execute(f"select id from dishes where name = ?", (dish, ))
+        cur.execute("select id from dishes where name = ?", (dish, ))
         dish_id = cur.fetchone()[0]
 
         dish_ingredients = dishes[dish]["ingredients"].replace(" or ", ", ").replace(".", "").replace(" and ", ", ").split(", ")
@@ -43,10 +43,14 @@ with open("WorldFood/dishes.json", "r", encoding="utf-8") as dishes_file:
             if ingredient == "":
                 continue
 
-            cur.execute(f"select id from ingredients where name = ?", (ingredient, ))
+            cur.execute("select id from ingredients where name = ?", (ingredient, ))
             ingredient_id = cur.fetchone()[0]
 
-            cur.execute(f"insert into dish_ingredients (dish_id, ingredient_id) values (?, ?)", (dish_id, ingredient_id))
+            cur.execute("insert into dish_ingredients (dish_id, ingredient_id) values (?, ?)", (dish_id, ingredient_id))
+
+# create liked_ingredients table
+cur.execute("DROP TABLE IF EXISTS liked_ingredients")
+cur.execute("create table liked_ingredients (id INTEGER PRIMARY KEY, username TEXT, ingredient_id INTEGER)")
 
 cur.close()
 conn.commit()
